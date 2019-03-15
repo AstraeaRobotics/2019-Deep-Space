9dc16557b41cdaa887164b497d511bd6283b866d
benwang2 <42283881+benwang2@users.noreply.github.com>
2019-03-15 17:37:44 -0400

    ree

--- ComputerVision/BallRecogV2.py
+++ ComputerVision/BallRecogV2.py
@@ -3,7 +3,9 @@ import numpy as np
 import keyboard
 import math
 import random
-#cap = cv2.VideoCapture(0)
+from networktables import NetworkTables
+
+# maps one val range to another
 def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Figure out how 'wide' each range is
     leftSpan = leftMax - leftMin
@@ -16,7 +18,7 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
     return rightMin + (valueScaled * rightSpan)
 
 
-
+# keeps value between low and hi val
 def clamp(innn,lo,hi):
     if(innn>=lo and innn<=hi):
         return innn
@@ -26,6 +28,7 @@ def clamp(innn,lo,hi):
         if(innn>hi):
             return hi
 
+#Iterates through circle and finds one with largest Radius
 def lgst(listCirc):
     maxV = listCirc[0][2]
     maxIndx = 0
@@ -37,6 +40,7 @@ def lgst(listCirc):
         cnt+=1
     return maxIndx
 
+#Magic Code that makes image brighter and darker
 def adjust_gamma(image, gamma=1.0):
 	# build a lookup table mapping the pixel values [0, 255] to
 	# their adjusted gamma values
@@ -48,39 +52,55 @@ def adjust_gamma(image, gamma=1.0):
 	return cv2.LUT(image, table)
 
 
+# Keep the value 0 for Laptop, 1 for Logitech Cameras
+cap = cv2.VideoCapture(0)
+NetworkTables.initialize(server='rasppifront')
+Vision = NetworkTables.getTable('SmartDashboard')
 
-cap = cv2.VideoCapture(1)
+move_Direction = 0
+#Allowance For Ball Centering
+center_Thresh = 120
 
+# Image Blur Vals - MUST BE ODD NON-ZERO INTEGER
+blurVal_1 = 5
+blurVal_2 = 1
+
+# Keyboard pressed Exit Loop
 while not keyboard.is_pressed('esc'):
+    move_Direction = 5
+
     #Inital Video Capture read
     _,frame = cap.read()
 
     #Randomly adjusts Gamma values to average out Dark and LIght Enviornments
-    frame = adjust_gamma(frame, gamma=clamp(3*random.random(),0.7,2.5))
+    #frame = adjust_gamma(frame, gamma=clamp(3*random.random(),0.7,2.5))
+
+    # 0 to 1 = dark \\ 1+ = brighter
+    frame = adjust_gamma(frame, gamma=3)
     image = frame.copy()
 
     #Gets Height and Width
     h,w,_ = image.shape
 
     #Creates a blurred HSV image from BGR imput image
-    hsv = cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV),(5,5),0)
+    hsv = cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV),(blurVal_1,blurVal_1),0)
 
     #Keep values between [(1-3), (70-100), (70-100)]
-    lowerOrange = np.array([2, 80, 80])
+    lower_Orange = np.array([2, 80, 80])
     #Keep values between [(15-19), (250-255), (250-255)]
-    upperOrange = np.array([17, 255, 255])
+    upper_Orange = np.array([17, 255, 255])
 
     #Masks all values outside Orange Ranges
-    tmask = cv2.inRange(hsv,lowerOrange,upperOrange)
-    mask = cv2.cvtColor(tmask, cv2.COLOR_GRAY2BGR)
+    temp_Mask = cv2.inRange(hsv,lower_Orange,upper_Orange)
+    overlay_Mask = cv2.cvtColor(temp_Mask, cv2.COLOR_GRAY2BGR)
 
     #applys mask to image
-    tColorMask = cv2.bitwise_and(frame, mask)
-    colorMask = cv2.cvtColor(tColorMask,cv2.COLOR_BGR2GRAY)
+    temp_ColorMask = cv2.bitwise_and(frame, overlay_Mask)
+    colorMask = cv2.cvtColor(temp_ColorMask,cv2.COLOR_BGR2GRAY)
 
     #Creates different blurry Adaptive threshed images
     threshImg1 = cv2.adaptiveThreshold(colorMask,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-    threshImg2 = cv2.adaptiveThreshold(cv2.GaussianBlur(colorMask,(1,1),0), 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+    threshImg2 = cv2.adaptiveThreshold(cv2.GaussianBlur(colorMask,(blurVal_2,blurVal_2),0), 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
     #threshImg3 = cv2.adaptiveThreshold(cv2.GaussianBlur(colorMask,(5,5),0), 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
 
 
@@ -88,7 +108,7 @@ while not keyboard.is_pressed('esc'):
     avgY = 0
     avgR = 0
 
-    cntt = 0
+    count_Circ = 0
     imgList = np.array([threshImg1, threshImg2])
 
     #Runs through all the added Images
@@ -96,35 +116,46 @@ while not keyboard.is_pressed('esc'):
         circles = cv2.HoughCircles(i,cv2.HOUGH_GRADIENT,1.2,5, param1=210,param2=135,minRadius=1,maxRadius = 200)
 
         if circles is not None:
-            cntt+=1
+            count_Circ+=1
 
             #Magic shit, no touchy
             circles = np.round(circles[0, :]).astype("int")
 
             #Gets the largest value
-            circL = lgst(circles)
-            x=circles[circL][0]
-            y=circles[circL][1]
-            r=circles[circL][2]
+            biggest_Circle = lgst(circles)
+            x=circles[biggest_Circle][0]
+            y=circles[biggest_Circle][1]
+            r=circles[biggest_Circle][2]
 
             avgR+=r
             avgX+=x
             avgY+=y
 
-            cv2.circle(image, (math.floor(avgX), math.floor(avgY)), math.floor(avgR), (255,255,0), 5)
+            #cv2.circle(image, (math.floor(avgX), math.floor(avgY)), math.floor(avgR), (255,255,0), 5)
 
-    if(not cntt==0):
-        avgR/=cntt
-        avgY/=cntt
-        avgX/=cntt
+    if(not count_Circ==0):
+        avgR/=count_Circ
+        avgY/=count_Circ
+        avgX/=count_Circ
+        # not -1, 0 or 1 for robot to get no input
+        move_Direction = 5
+        if(avgX<(w/2)-center_Thresh):
+            move_Direction = 1
+        if(avgX>(w/2)+center_Thresh):
+            move_Direction = -1
+        if(avgX>(w/2)-center_Thresh and avgX<(w/2)+center_Thresh):
+            move_Direction = 0
 
-        cv2.circle(image, (math.floor(avgX), math.floor(avgY)), math.floor(avgR), (0,255,255), 3)
 
-        print(translate(avgX,0,w,0,78))
 
-    cv2.imshow('masked Image', tColorMask)
-    cv2.imshow('threshed',threshImg2)
-    cv2.imshow('lotta Circz',image)
+
+        #cv2.circle(image, (math.floor(avgX), math.floor(avgY)), math.floor(avgR), (0,0,0), 3)
+
+    Vision.putNumber('LeftRightValue', move_Direction)
+    #print(move_Direction)
+    #cv2.imshow('masked Image', tColorMask)
+    #cv2.imshow('threshed',threshImg2)
+    #cv2.imshow('lotta Circz',image)
 
     cv2.waitKey(1)
 
